2dc4efc3e70f5a646df4376df4889bb36c84aa43
jay <jay@jeepies.codes>
2024-11-19 18:28:06 +0000

    formatting app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ import routes from "./routes";
 const app: Express = express();
 
 Object.entries(routes).forEach(([route, handlers]) => {
-    if(route === "root") route = ""
-    app.use(`/api/${route}`, handlers)
-})
+  if (route === "root") route = "";
+  app.use(`/api/${route}`, handlers);
+});
 
 export default app;
