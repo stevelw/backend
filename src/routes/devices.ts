57a92ea5a62d7fb327f87d035f2fd6f93774e8e5
jay <jay@jeepies.codes>
2024-11-19 18:52:58 +0000

    added /api/devices/create

--- src/routes/devices.ts
+++ src/routes/devices.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import * as controller from "../controllers/devices";
 
 const devices: Router = Router();
 
-devices.post("/create")
+devices.post("/create", controller.createDevice);
 
 export default devices;
