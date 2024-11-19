477cc7eda4e0f132ec3a794dbeabdc6658d6d1dc
jay <jay@jeepies.codes>
2024-11-19 17:27:14 +0000

    primitive tests for route that views all users devices

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,22 +1,47 @@
-import app from "../src/app";
-import server from "../src/server";
-import request from "supertest";
+import app from '../src/app';
+import server from '../src/server';
+import request from 'supertest';
 
 afterAll(() => {
-  server.close();
+	server.close();
 });
 
-describe("🧪 Express Application", () => {
-  describe("GET /api", () => {
-    it("200: should return a successful response", () => {
-      return request(app)
-        .get("/api")
-        .expect(200)
-        .then(({ body }) => {
-          expect(body).toEqual({
-            success: true,
-          });
-        });
-    });
-  });
+describe('🧪 Express Application', () => {
+	describe('GET /api', () => {
+		it('200: should return a successful response', () => {
+			return request(app)
+				.get('/api')
+				.expect(200)
+				.then(({ body }) => {
+					expect(body).toEqual({
+						success: true,
+					});
+				});
+		});
+	});
+
+	describe('GET /api/users/:id/devices', () => {
+		const userWithDevices = 'cm3op7iwu0000jrcqa60tc9kv';
+		const userWithoutDevices = 'cm3op7iww0001jrcqpq3qxx6i';
+
+		it('200: should return the devices for a valid user with more than one device', () => {
+			return request(app)
+				.get(`/api/users/${userWithDevices}/devices`)
+				.expect(200)
+				.then(({ body: { success, data } }) => {
+					expect(success).toBe(true);
+					expect(data).not.toEqual([]);
+				});
+		});
+
+		it('200: should return an empty array when given a valid user with no devices', () => {
+			return request(app)
+				.get(`/api/users/${userWithoutDevices}/devices`)
+				.expect(200)
+				.then(({ body: { success, data } }) => {
+					expect(success).toBe(true);
+					expect(data).toEqual([]);
+				});
+		});
+	});
 });
